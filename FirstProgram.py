754a4f3bfa8cb4e708ccaffbc31119643a1a389a
joshfruge1 <joshfruge1997@outlook.com>
2024-08-31 17:11:00 -0500

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,14 +6,18 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  name = input("What is your name?")
+  print(name) 
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("Enter your age: ")
+  age = int(age) #Convert from words to number
   #Tell the user what year they were born in.
+  born = 2024 - age
+  print("You were born in", born)
   #Assume that they have not had their birthday yet this year.
 
 
